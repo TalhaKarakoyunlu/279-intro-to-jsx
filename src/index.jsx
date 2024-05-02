061fde94e2ce747994ce242b0e53a19ca6039829
Talha Bahadır Karakoyunlu <karakoyunlutalha34@gmail.com>
2024-05-03 02:32:18 +0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a test.</p>
+    <h2>Seems to be working.</h2>
+    <h3>Noice.</h3>
+  </div>,
+  document.getElementById("root")
+);
